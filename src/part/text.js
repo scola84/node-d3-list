4a4dd98e652031d0eb0f6ba3a042eeedf339811a
M.C. van den Boogaart <scola@kvdb.name>
2017-07-31 12:03:55 +0000

    Check if value is undefined in text part

--- src/part/text.js
+++ src/part/text.js
@@ -215,6 +215,6 @@ export default class Text extends Part {
     }
 
     const value = this._format(setEvent.value);
-    this.text(value === null ? '' : value);
+    this.text(typeof value === 'undefined' ? '' : value);
   }
 }
